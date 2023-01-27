b70eedf19ec423aae4e96c29160c05fe2c0aa809
Shamah M Zoha <shamah.zoha@gmail.com>
2023-01-28 02:10:46 +0600

    Added AppRunner class to run application

---
+++ tranxacto-account/src/main/java/com/zedapps/txaccount/AppRunner.java
@@ -0,0 +1,16 @@
+package com.zedapps.txaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Shamah M Zoha
+ * @since 27-Jan-23
+ */
+@SpringBootApplication
+public class AppRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRunner.class, args);
+    }
+}
