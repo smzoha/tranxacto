68f8e85d74ca07ea8f6f697bfa59f88a9b577007
Shamah M Zoha <shamah.zoha@gmail.com>
2023-03-10 14:24:45 +0600

    Added SupportingDocumentDto in Common API
    A DTO representing Supporting Document has been added in Common API
    which allows for two-fold advantage - to be able to pass a user-readable
    representation of the original entity as response of API, and
    encapsulating the original data when shown to users.

---
+++ common-api/src/main/java/com/zedapps/common/dto/SupportingDocumentDto.java
@@ -0,0 +1,32 @@
+package com.zedapps.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author Shamah M Zoha
+ * @since 10-Mar-23
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SupportingDocumentDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private long id;
+
+    private String name;
+
+    private String size;
+
+    private Date uploadDate;
+
+}
