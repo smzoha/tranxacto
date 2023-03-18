6b0bcb8ef36c29dad1274dbb51101d105d0f82e1
Shamah M Zoha <shamah.zoha@gmail.com>
2023-03-18 17:07:25 +0600

    Added SupportingDocumentService Feign Client in Account API
    In order to access the Supporting Document API from the Account API, a
    FeignClient is created, which refers to the required endpoints of the
    API.
    
    The functionalities include uploading and downloading files, removing
    them, along with retrieving information of the file.
    
    The FeignClient annotation contains the service name, along with the
    context path, in order to make the requests properly.

---
+++ tranxacto-account/src/main/java/com/zedapps/txaccount/service/SupportingDocumentService.java
@@ -0,0 +1,28 @@
+package com.zedapps.txaccount.service;
+
+import com.zedapps.common.dto.SupportingDocumentDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author Shamah M Zoha
+ * @since 18-Mar-23
+ */
+
+@FeignClient(value = "supporting-docs-api", path = "/docs")
+public interface SupportingDocumentService {
+
+    @PostMapping("/upload")
+    public SupportingDocumentDto uploadDocument(@RequestParam MultipartFile file);
+
+    @GetMapping("/info/{id}")
+    public SupportingDocumentDto getDocumentInfo(@PathVariable long id);
+
+    @GetMapping("/download/{id}")
+    public ResponseEntity<?> downloadDocument(@PathVariable long id);
+
+    @DeleteMapping("/delete/{id}")
+    public String removeFile(@PathVariable long id);
+}
