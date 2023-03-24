e84f803ba945194b9fdcc2e217632d67069bf19c
Shamah M Zoha <shamah.zoha@gmail.com>
2023-03-24 23:02:17 +0600

    Replaced AccountRepository usage with service in AccountController

--- tranxacto-account/src/main/java/com/zedapps/txaccount/controller/AccountController.java
+++ tranxacto-account/src/main/java/com/zedapps/txaccount/controller/AccountController.java
@@ -1,7 +1,7 @@
 package com.zedapps.txaccount.controller;
 
 import com.zedapps.txaccount.entity.Account;
-import com.zedapps.txaccount.repository.AccountRepository;
+import com.zedapps.txaccount.service.AccountService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,21 +21,21 @@ import java.util.Optional;
 public class AccountController {
 
     @Autowired
-    private AccountRepository accountRepository;
+    private AccountService accountService;
 
     @GetMapping("/all")
     public List<Account> getAccounts() {
-        return accountRepository.findAll();
+        return accountService.getAccounts();
     }
 
     @GetMapping("/active")
     public List<Account> getActiveAccounts() {
-        return accountRepository.findAllActiveAccounts();
+        return accountService.getActiveAccounts();
     }
 
     @GetMapping("/{id}")
     public Account getAccount(@PathVariable long id) {
-        Optional<Account> account = accountRepository.findById(id);
+        Optional<Account> account = accountService.getAccount(id, true);
 
         if (account.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Account not found by id!");
@@ -46,12 +46,12 @@ public class AccountController {
 
     @PostMapping(value = "/save")
     public Account saveAccount(@Valid @RequestBody Account account) {
-        return accountRepository.save(account);
+        return accountService.saveOrUpdateAccount(account);
     }
 
     @PostMapping(value = "/update/{id}")
     public Account updateAccount(@Valid @RequestBody Account account, @PathVariable long id) {
-        Optional<Account> origAccount = accountRepository.findById(id);
+        Optional<Account> origAccount = accountService.getAccount(id, false);
 
         if (origAccount.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Invalid id passed for update!");
@@ -59,18 +59,18 @@ public class AccountController {
 
         account.setId(origAccount.get().getId());
 
-        return accountRepository.save(account);
+        return accountService.saveOrUpdateAccount(account);
     }
 
     @DeleteMapping(value = "/delete/{id}")
     public ResponseEntity<Object> deleteAccount(@PathVariable long id) {
-        Optional<Account> account = accountRepository.findById(id);
+        Optional<Account> account = accountService.getAccount(id, false);
 
         if (account.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Invalid id passed for deletion!");
         }
 
-        accountRepository.delete(account.get());
+        accountService.deleteAccount(account.get());
 
         return ResponseEntity.ok("Successfully deleted!");
     }
