67dbb76466d41c0372009df9d2f1e8332226d981
Shamah M Zoha <shamah.zoha@gmail.com>
2023-04-19 17:19:20 +0600

    Added Login entity
    In addition, the Role enum class has been made, which includes role to
    assign for four types of users:
    * Super Admin - user who is able to perform all actions there are
    * Admin - user who is able to manage user privileges & change user's
      preferences
    * Account - user who is able to access account functionality
    * Transaction - user who is able to access transaction functionality (to
    be implemented)
    
    Also, the Status enum class has been introduced, with the following
    items: active, inactive, blocked & deleted.

---
+++ tranxacto-auth/src/main/java/com/zedapps/txauth/entity/enums/Role.java
@@ -0,0 +1,21 @@
+package com.zedapps.txauth.entity.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author Shamah M Zoha
+ * @since 19-Apr-23
+ */
+
+@Getter
+@AllArgsConstructor
+public enum Role {
+
+    SUPER_ADMIN("Super Admin"),
+    ADMIN("Admin"),
+    ACCOUNT("Account Access"),
+    TRANSACTION("Transaction Access");
+
+    private final String naturalName;
+}
