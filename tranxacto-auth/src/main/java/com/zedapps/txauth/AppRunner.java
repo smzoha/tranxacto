9efb85e3c99e0166261343f0af25746a5f555dad
Shamah M Zoha <shamah.zoha@gmail.com>
2023-04-19 17:00:32 +0600

    Added AppRunner class to run the API

---
+++ tranxacto-auth/src/main/java/com/zedapps/txauth/AppRunner.java
@@ -0,0 +1,16 @@
+package com.zedapps.txauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Shamah M Zoha
+ * @since 19-Apr-23
+ */
+@SpringBootApplication
+public class AppRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRunner.class, args);
+    }
+}
