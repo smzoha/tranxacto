0e6ca15bdd8c692e01562bde8d63334bbd71b9b4
Shamah M Zoha <shamah.zoha@gmail.com>
2023-05-01 22:16:48 +0600

    Added LoginService to Auth API
    LoginService has been implemented, which allows for retrieval of
    logins/users from the database. Included methods allows for retrieving
    all logins, retrieving only active logins, retrieving login by username,
    saving login from a LoginRequestDto.
    
    Also included is a mapper method which is used to convert a Login object
    to LoginResponseDto.

---
+++ tranxacto-auth/src/main/java/com/zedapps/txauth/service/LoginService.java
@@ -0,0 +1,75 @@
+package com.zedapps.txauth.service;
+
+import com.zedapps.common.dto.LoginRequestDto;
+import com.zedapps.common.dto.LoginResponseDto;
+import com.zedapps.common.util.ResponseUtils;
+import com.zedapps.txauth.entity.Login;
+import com.zedapps.txauth.entity.enums.Role;
+import com.zedapps.txauth.repository.LoginRepository;
+import jakarta.transaction.Transactional;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * @author Shamah M Zoha
+ * @since 01-May-23
+ */
+
+@Service
+public class LoginService {
+
+    @Autowired
+    private LoginRepository loginRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public List<LoginResponseDto> getAllLogins() {
+        return loginRepository.findAll()
+                .stream()
+                .map(LoginService::generateLoginResponse)
+                .collect(Collectors.toList());
+    }
+
+    public List<LoginResponseDto> getActiveLogins() {
+        return loginRepository.getActiveLogins()
+                .stream()
+                .map(LoginService::generateLoginResponse)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<Login> getLogin(String username) {
+        return loginRepository.findByUsername(username);
+    }
+
+    public LoginResponseDto getLoginResponse(String username) {
+        Optional<Login> login = getLogin(username);
+
+        return login.map(LoginService::generateLoginResponse).orElse(null);
+    }
+
+    @Transactional
+    public LoginResponseDto saveOrUpdateAccount(LoginRequestDto loginRequestDto) {
+        Login login = new Login(loginRequestDto);
+
+        if (StringUtils.isNotBlank(loginRequestDto.getPlainPassword())) {
+            login.setPassword(passwordEncoder.encode(loginRequestDto.getPlainPassword()));
+        }
+
+        loginRepository.save(login);
+
+        return generateLoginResponse(login);
+    }
+
+    private static LoginResponseDto generateLoginResponse(Login login) {
+        return ResponseUtils.getLoginResponse(login.getUsername(), login.getEmail(), login.getFirstName(),
+                login.getLastName(), login.getStatus().name(),
+                login.getRoles().stream().map(Role::name).collect(Collectors.toSet()));
+    }
+}
