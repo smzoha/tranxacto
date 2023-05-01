6100d1c6f05d245ac098064d2bc996b7c5788b0f
Shamah M Zoha <shamah.zoha@gmail.com>
2023-05-01 22:16:48 +0600

    Added LoginService to User API
    LoginService has been implemented, which allows for retrieval of
    logins/users from the database. Included methods allows for retrieving
    all logins, retrieving only active logins, retrieving login by username,
    saving login from a LoginRequestDto.
    
    Also included is a mapper method which is used to convert a Login object
    to LoginResponseDto.

--- tranxacto-user/src/main/java/com/zedapps/txuser/AppRunner.java
+++ tranxacto-user/src/main/java/com/zedapps/txuser/AppRunner.java
@@ -2,6 +2,8 @@ package com.zedapps.txuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @author Shamah M Zoha
@@ -12,5 +14,10 @@ public class AppRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(AppRunner.class, args);
+    }
+
+    @Bean("passwordEncoder")
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
